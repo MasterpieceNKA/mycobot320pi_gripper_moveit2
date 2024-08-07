84ca7fb9b50b4bfec4dd35bf2359db15bf4c851c
MasterpieceNKA <nemwel.keoro@gmail.com>
2024-08-07 08:45:49 +0100

    updated gripper urdf and moveit config files

---
+++ demo_mycobot_320pi/launch/demo.launch.py
@@ -0,0 +1,168 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node 
+from launch.substitutions import LaunchConfiguration
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+from moveit_configs_utils.launch_utils import (
+    add_debuggable_node,
+    DeclareBooleanLaunchArg,
+)
+from launch.actions import DeclareLaunchArgument
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+    ld.add_action(DeclareBooleanLaunchArg("use_rviz", default_value=True))
+    ld.add_action(DeclareLaunchArgument("publish_frequency", default_value="15.0"))
+    
+    # MoveGroup parameters
+    ld.add_action(DeclareBooleanLaunchArg("debug", default_value=False))
+    ld.add_action(
+        DeclareBooleanLaunchArg("allow_trajectory_execution", default_value=True)
+    )
+    ld.add_action(
+        DeclareBooleanLaunchArg("publish_monitored_planning_scene", default_value=True)
+    )
+    '''
+    # load non-default MoveGroup capabilities (space separated)
+    ld.add_action(
+        DeclareLaunchArgument(
+            "capabilities",
+            default_value=moveit_config.move_group_capabilities["capabilities"],
+        )
+    )
+    # inhibit these default MoveGroup capabilities (space separated)
+    ld.add_action(DeclareLaunchArgument("disable_capabilities", default_value=""))
+    '''
+
+    # do not copy dynamics information from /joint_states to internal robot monitoring
+    # default to false, because almost nothing in move_group relies on this information
+    ld.add_action(DeclareBooleanLaunchArg("monitor_dynamics", default_value=False))
+
+    should_publish = LaunchConfiguration("publish_monitored_planning_scene")
+
+
+    moveit_config = MoveItConfigsBuilder("firefighter", package_name="mycobot_320pi_moveit_config").to_moveit_configs()
+
+    # broadcast static tf of virtual_joints
+    ld.add_action(
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name=f"static_transform_publisher",
+            output="log",
+            arguments=[
+                "--frame-id",
+                "world",
+                "--child-frame-id",
+                "base",
+            ],
+        )
+    )
+    # Given the published joint states, publish tf for the robot links
+    ld.add_action(
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            respawn=True,
+            output="screen",
+            parameters=[
+                moveit_config.robot_description, #robot_description, #
+                {
+                    "publish_frequency": LaunchConfiguration("publish_frequency"),
+                },
+            ],
+        )
+    )
+
+    # MoveGroup settings
+    move_group_configuration = {
+        "publish_robot_description_semantic": True,
+        "allow_trajectory_execution": LaunchConfiguration("allow_trajectory_execution"),
+        # Note: Wrapping the following values is necessary so that the parameter value can be the empty string
+        #"capabilities": ParameterValue(LaunchConfiguration("capabilities"), value_type=str),
+        #"disable_capabilities": ParameterValue(LaunchConfiguration("disable_capabilities"), value_type=str),
+        # Publish the planning scene of the physical robot so that rviz plugin can know actual robot
+        "publish_planning_scene": should_publish,
+        "publish_geometry_updates": should_publish,
+        "publish_state_updates": should_publish,
+        "publish_transforms_updates": should_publish,
+        "monitor_dynamics": False,
+    }
+    move_group_params = [
+        moveit_config.to_dict(),
+        move_group_configuration,
+    ]
+
+    add_debuggable_node(
+        ld,
+        package="moveit_ros_move_group",
+        executable="move_group",
+        commands_file=str(moveit_config.package_path / "launch" / "gdb_settings.gdb"),
+        output="screen",
+        parameters=move_group_params,
+        extra_debug_args=["--debug"],
+        # Set the display variable, in case OpenGL code is used internally
+        additional_env={"DISPLAY": os.environ["DISPLAY"]},
+    )
+
+    # Run Rviz and load the default config to see the state of the move_group node
+    ld.add_action(
+        DeclareLaunchArgument(
+            "rviz_config",
+            default_value=PathJoinSubstitution([
+                    FindPackageShare('demo_mycobot_320pi'),
+                    'config',
+                    'moveit.rviz'
+                ]) #str(moveit_config.package_path / "config/moveit.rviz"),
+        )
+    )
+
+    rviz_parameters = [
+        moveit_config.planning_pipelines,
+        moveit_config.robot_description_kinematics,
+    ]
+
+    add_debuggable_node(
+        ld,
+        package="rviz2",
+        executable="rviz2",
+        output="log",
+        respawn=False,
+        arguments=["-d", LaunchConfiguration("rviz_config")],
+        parameters=rviz_parameters,
+    )
+
+    # Fake joint driver
+    ld.add_action(
+        Node(
+            package="controller_manager",
+            executable="ros2_control_node",
+            parameters=[
+                moveit_config.robot_description,
+                str(moveit_config.package_path / "config/ros2_controllers.yaml"),
+            ],
+        )
+    )
+
+    # Controllers
+    controller_names = moveit_config.trajectory_execution.get(
+        "moveit_simple_controller_manager", {}
+    ).get("controller_names", []) 
+    for controller in controller_names + ["joint_state_broadcaster"]:
+        ld.add_action(
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[controller],
+                output="screen",
+            )
+        )
+
+
+    return ld
---
+++ demo_mycobot_320pi/src/demo_mycobot_320pi_moveit.cpp
@@ -0,0 +1,13 @@
+#include <memory>
+#include <iostream>
+
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h> 
+
+int main(int argc, char * argv[])
+{
+
+    std::cout << "Hello from demo!!\n";
+
+    return 0;
+}
