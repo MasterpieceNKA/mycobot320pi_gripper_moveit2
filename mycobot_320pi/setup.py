aa96c430c5c3f00d5efe1ac3ce4b05865be6dcfb
MasterpieceNKA <nemwel.keoro@gmail.com>
2024-09-13 19:57:52 +0100

    debuged packages.xml

--- mycobot_320pi/setup.py
+++ mycobot_320pi/setup.py
@@ -13,11 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # launch 文件路径
         (os.path.join('share', package_name, "launch"), glob('launch/*.launch.py')),
-        # python 文件
         # (os.path.join('lib',package_name),glob(package_name+'/*.py')),
-        # 配置文件
         (os.path.join('share', package_name, "config"), glob('config/*')),
         ('share/' + package_name + '/urdf',glob("urdf/*")),
         ('share/' + package_name + '/meshes/visual',glob("meshes/visual/*")),
@@ -32,17 +29,6 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-            'camera_display = mycobot_320pi.camera_display:main',
-            'detect_marker = mycobot_320pi.detect_marker:main',
-            'follow_display = mycobot_320pi.follow_display:main',
-            'following_marker = mycobot_320pi.following_marker:main',
-            'listen_real_of_topic = mycobot_320pi.listen_real_of_topic:main',
-            'listen_real = mycobot_320pi.listen_real:main',
-            'opencv_camera = mycobot_320pi.opencv_camera:main',
-            'simple_gui = mycobot_320pi.simple_gui:main',
-            'slider_control = mycobot_320pi.slider_control:main',
-            'teleop_keyboard = mycobot_320pi.teleop_keyboard:main',
-        ],
+        'console_scripts': [ ],
     },
 )
